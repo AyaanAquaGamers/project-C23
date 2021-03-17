9801af957576b58d2a2b0c6220ec05cd93a23e37
AyaanAquaGamers <72394681+AyaanAquaGamers@users.noreply.github.com>
2021-03-17 16:35:44 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,4 @@
-var paper
-var ground
-var leftSide
-var bottom
-var rightSide
-
+var roof,rope1,bobObject1,bobObject2,bobObject3,bobObject4,bobObject5,bobDiameter
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -17,16 +12,37 @@ function preload()
 function setup() {
 	createCanvas(800, 700);
 
+
 	engine = Engine.create();
 	world = engine.world;
 
-  paper = new Paper(100,600,30) 
-  
-  ground = new Ground(400,650,800,20)
+   roof=new Roof(350,100,300,30)
+   World.add(world,roof)
 
- leftSide=createSprite(570,620,20,40)
- bottom=createSprite(650,630,180,20)
- rightSide=createSprite(750,620,20,40)
+   bobObject1=new Bob(250,300)
+   bobObject2=new Bob(300,300)
+   bobObject3=new Bob(350,300)
+   bobObject4=new Bob(400,300)
+   bobObject5=new Bob(450,300)
+
+
+
+   rope1=new Rope(bobObject5.body,roof.body,100,0)
+   World.add(world,rope1)
+
+   rope2=new Rope(bobObject4.body,roof.body,50,0)
+   World.add(world,rope2)
+
+   rope3=new Rope(bobObject3.body,roof.body,0,0)
+   World.add(world,rope3)
+
+   rope4=new Rope(bobObject2.body,roof.body,-50,0)
+   World.add(world,rope4)
+
+   rope5=new Rope(bobObject1.body,roof.body,-100,0)
+   World.add(world,rope5)
+                         
+
 
 
 	Engine.run(engine);
@@ -36,23 +52,34 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-
-  background("pink");
-
+  background("white");
   Engine.update(engine)
+  
+  
+  roof.display()
 
-  ground.display()
 
-  paper.display()
+  bobObject1.display()
+  bobObject2.display()
+  bobObject3.display()
+  bobObject4.display()
+  bobObject5.display()
 
+
+  rope1.display()
+  rope2.display()
+  rope3.display()
+  rope4.display()
+  rope5.display()
 
   drawSprites();
  
 }
 
-
 function keyPressed(){
-	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:23,y:-50})
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-730,y:0})
 	}
 }
+
+
